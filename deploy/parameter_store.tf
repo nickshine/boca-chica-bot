bae19541471270bbf0e3c2c4dcf65eb7ac9f373f
Nick Shine <shine.nick@gmail.com>
2020-12-03 22:32:35 -0600

    refactor: for_each on parameter store creds

--- deploy/parameter_store.tf
+++ deploy/parameter_store.tf
@@ -3,34 +3,21 @@ variable "twitter_consumer_secret" {} // provided in Terraform Cloud workspace
 variable "twitter_access_token" {}    // provided in Terraform Cloud workspace
 variable "twitter_access_secret" {}   // provided in Terraform Cloud workspace
 
-resource "aws_ssm_parameter" "twitter_consumer_key" {
-  name  = "${var.param_store_path}/twitter_consumer_key"
-  type  = "SecureString"
-  value = var.twitter_consumer_key
-
-  tags = local.tags
+locals {
+  params = {
+    "consumer_key"    = var.twitter_consumer_key
+    "consumer_secret" = var.twitter_consumer_secret
+    "access_token"    = var.twitter_access_token
+    "access_secret"   = var.twitter_access_secret
+  }
 }
 
-resource "aws_ssm_parameter" "twitter_consumer_secret" {
-  name  = "${var.param_store_path}/twitter_consumer_secret"
+resource "aws_ssm_parameter" "twitter_creds" {
+  for_each = local.params
+
+  name  = "${var.param_store_path}/twitter_${each.key}"
   type  = "SecureString"
-  value = var.twitter_consumer_secret
-
-  tags = local.tags
-}
-
-resource "aws_ssm_parameter" "twitter_access_token" {
-  name  = "${var.param_store_path}/twitter_access_token"
-  type  = "SecureString"
-  value = var.twitter_access_token
-
-  tags = local.tags
-}
-
-resource "aws_ssm_parameter" "twitter_access_secret" {
-  name  = "${var.param_store_path}/twitter_access_secret"
-  type  = "SecureString"
-  value = var.twitter_access_secret
+  value = each.value
 
   tags = local.tags
 }
