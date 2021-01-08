4b4c10dd213796ae54f800dc837c3084f4468cd2
Nick Shine <shine.nick@gmail.com>
2021-01-08 00:25:48 -0600

    refactor: reduce discord logging

--- cmd/publisher/discord.go
+++ cmd/publisher/discord.go
@@ -27,7 +27,6 @@ func handleDiscord(params map[string]string, messages []string) error {
 	log.Debugf("guild server count: %d", len(guilds))
 
 	for _, g := range guilds {
-		log.Debugf("processing guild server: %s", g.Name)
 		channels, err := session.GuildChannels(g.ID)
 		if err != nil {
 			log.Debugf("channel retrieval error: %v", err)
@@ -38,12 +37,9 @@ func handleDiscord(params map[string]string, messages []string) error {
 				log.Debugf("Guild name: %s - Channel: %s", g.Name, ch.Name)
 				for _, m := range messages {
 					log.Debugf("sending message: %s", m)
-					msg, err := session.ChannelMessageSend(ch.ID, m)
+					_, err := session.ChannelMessageSend(ch.ID, m)
 					if err != nil {
 						log.Debugf("channel send error: %v", err)
-					}
-					if msg != nil {
-						log.Debugf("Discord response: %+v", msg)
 					}
 				}
 			}
